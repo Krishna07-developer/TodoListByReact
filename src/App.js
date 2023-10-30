49f629e89ea15c42f27e589999b38be273fd14aa
unpairedkrish-7 <krishnabhukya893@gmail.com>
2023-10-30 19:54:19 +0530

    My TodoList First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState } from 'react';
 
 function App() {
+
+  const [todos,setTodos] = useState([]);
+
+  const [inputTodo,setInputTodo] = useState("");
+
+  const [filter,setFilter] = useState("All");
+
+
+  function handleAddTask(){
+    setTodos([...todos,{
+      task : inputTodo,
+      id : Date.now().toString(),
+      isCompleted : false
+    }]);
+    setInputTodo("")
+  }
+
+  function handleDelete(deletedTodoId){
+    const newTodos = todos.filter((oneTodo)=> oneTodo.id !== deletedTodoId);
+
+    setTodos(newTodos);
+  }
+  
+
+  function toggleCompleteTask(taskId){
+    const newTodos = todos.map((todo)=>{
+      if(todo.id === taskId){
+        return {
+          ...todo,
+          isCompleted : !todo.isCompleted
+        }
+      }
+      
+      return todo;
+      
+      
+    })
+    setTodos(newTodos);
+  }
+
+  const handleFilter=()=>{
+    if(filter === "Active"){
+      return todos.filter((todo)=>todo.isCompleted === false)
+    }else if(filter === "Completed"){
+      return todos.filter((todo)=>todo.isCompleted === true)
+    }else{
+      return todos;
+    }
+  }
+
+  const clearCompletdTask=()=>{
+    const activeTodos = todos.filter((todo)=>!todo.isCompleted);
+
+    setTodos(activeTodos)
+  }
+
+  const getActiveTodos =()=>{
+    const activeNumberTodo = todos.filter((todo)=> !todo.isCompleted);
+
+    return activeNumberTodo.length;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="todoBox">
+        <h1>Todo List :-</h1>
+        <input type="text" onChange={(e)=>setInputTodo(e.target.value)} value={inputTodo} className='inputBar'/>
+        <button onClick={handleAddTask} className='addBtn'>ADD</button><br/><hr/>
+        <div>
+          {
+            handleFilter().length > 0 ? handleFilter().map((todo,index)=>{
+                return (
+                  <div className='todo' key={index}>
+                    <input type="checkbox" checked={todo.isCompleted} onChange={()=>toggleCompleteTask(todo.id)}/>
+                    <span>{todo.task}</span>
+                    <button onClick={()=>handleDelete(todo.id)}>Delete</button><br/>
+                  </div>
+                )
+            }) : "No Task is There"
+          }
+        </div>
+        <div className='footerBtn'>
+          {getActiveTodos()} remaining Active Todos.
+          <button onClick={()=>setFilter("All")}>All</button>
+          <button onClick={()=>setFilter("Active")}>Active</button>
+          <button onClick={()=>setFilter("Completed")}>Completed</button>
+          <button onClick={clearCompletdTask}>ClearCompletedTask</button>
+        </div>
+      </div>
+    </section>
   );
 }
 
